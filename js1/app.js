a281d3d6724b6678b0bf6e1d7e6d03f8090ff3ef
Nihadname <114952145+Nihadname@users.noreply.github.com>
2023-06-11 15:13:12 -0700

    Add files via upload

---
+++ js1/app.js
@@ -0,0 +1,41 @@
+let x = 5;
+let y = 2;
+let z = x * y;
+
+
+let a = 3;
+let j = (100 + 50) * a;
+
+let answer1 = "It's alright";
+let answer2 = "He is called 'Johnny'";
+let answer3 = 'He is called "Johnny"';
+
+
+function myFunction() {
+  let text = document.getElementById("demo").innerHTML;
+  document.getElementById("demo").innerHTML =
+  text.replace("Microsoft","W3Schools");
+}  
+text = text.replaceAll(/Cats/g,"Dogs");
+text = text.replaceAll(/cats/g,"dogs");
+
+//A string is converted to upper case with toUpperCase():
+
+//A string is converted to lower case with toLowerCase():
+
+let text1 = "Hello World!";
+let text2 = text1.toUpperCase();
+
+let text5 = "Hello";
+let text3 = "World!";
+
+//write array which one is for cars   and one is for bikes
+
+let cars = ["BMW", "Volvo", "Saab", "Ford"];
+let bikes = ["Honda", "Yamaha", "Suzuki"];
+
+//write a function which will take two parameters and will return the sum of them
+
+
+
+
